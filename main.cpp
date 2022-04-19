3b0b0a60c926300a1cc7f907966fd65c046397fe
mango <2965531503@qq.com>
2022-04-19 21:23:34 +0800

    vscode配置launck.json+tasks.json+cmake

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "swap.h"
+
+using namespace std;
+
+int main()
+{
+    int b=0;
+    int a = 9;
+
+    cout << "ÖÐÎÄ" << endl;
+    cout << "aaa" << endl;
+    
+    cout << a << b << endl;
+    swap(a, b);
+    cout << a << b << endl;
+   
+
+    return 0;
+}
+
+
